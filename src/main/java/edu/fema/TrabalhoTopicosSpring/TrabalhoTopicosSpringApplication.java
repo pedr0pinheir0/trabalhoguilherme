5cd89c67e1c03f22b98a2231557a5970efbc5125
Gabriel Alan <alang.von@gmail.com>
2019-10-23 18:58:46 -0300

    Projeto Inicializado.

---
+++ src/main/java/edu/fema/TrabalhoTopicosSpring/TrabalhoTopicosSpringApplication.java
@@ -0,0 +1,13 @@
+package edu.fema.TrabalhoTopicosSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoTopicosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoTopicosSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/fema/TrabalhoTopicosSpring/TrabalhoTopicosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fema.TrabalhoTopicosSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoTopicosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
