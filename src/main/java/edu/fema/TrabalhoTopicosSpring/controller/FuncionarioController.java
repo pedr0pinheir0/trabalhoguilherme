99231d1dd4c16d491e5208a6025c1609d4de119d
Pedro <p3dro_pinheiro@hotmail.com>
2019-10-25 20:33:01 -0300

    controller atualizado

--- src/main/java/edu/fema/TrabalhoTopicosSpring/controller/FuncionarioController.java
+++ src/main/java/edu/fema/TrabalhoTopicosSpring/controller/FuncionarioController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +21,17 @@ public class FuncionarioController {
 
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	@GetMapping
 	public ResponseEntity<?> buscarTodos() {
 		return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.findAll());
 	}
-	
+
+	@GetMapping("/{codigo}")
+	public ResponseEntity<?> buscarPorCodigo(@PathVariable @Valid Long codigo) {
+		return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.findById(codigo));
+	}
+
 	@PostMapping
 	public ResponseEntity<?> cadastrarUsuario(@RequestBody @Valid Funcionario usuario) {
 		return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.save(usuario));
--- src/main/java/edu/fema/TrabalhoTopicosSpring/controller/PontoController.java
+++ src/main/java/edu/fema/TrabalhoTopicosSpring/controller/PontoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,18 +20,24 @@ public class PontoController {
 
 	@Autowired
 	private PontoRepository pontoRepository;
-	
-	@Autowired PontoService pontoService;
-	
+
+	@Autowired
+	PontoService pontoService;
+
 	@GetMapping
 	public ResponseEntity<?> buscarTodosOsPontos() {
 		return ResponseEntity.status(HttpStatus.OK).body(pontoRepository.findAll());
 	}
-	
+
+	@GetMapping("/{codigo}")
+	public ResponseEntity<?> buscarPontoPorCodigo(@PathVariable Long codigo) {
+		return ResponseEntity.status(HttpStatus.OK).body(pontoRepository.findById(codigo));
+	}
+
 	@PostMapping
 	public ResponseEntity<?> efetuarRegistro(@RequestParam Long idFuncionario) {
 		Ponto ponto = pontoService.efetuarRegistro(idFuncionario);
-		if(ponto == null) {
+		if (ponto == null) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		} else {
 			return ResponseEntity.status(HttpStatus.OK).body(ponto);
