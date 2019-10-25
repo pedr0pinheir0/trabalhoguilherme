5d0f1762480fef0988cf822f87fa76dc35a1dcd8
Gabriel Alan <alang.von@gmail.com>
2019-10-25 20:26:33 -0300

    Entrada e Saída

--- src/main/java/edu/fema/TrabalhoTopicosSpring/service/PontoService.java
+++ src/main/java/edu/fema/TrabalhoTopicosSpring/service/PontoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import edu.fema.TrabalhoTopicosSpring.model.Funcionario;
 import edu.fema.TrabalhoTopicosSpring.model.Ponto;
+import edu.fema.TrabalhoTopicosSpring.model.enums.TipoPontoEnum;
 import edu.fema.TrabalhoTopicosSpring.repository.PontoRepository;
 import edu.fema.TrabalhoTopicosSpring.repository.UsuarioRepository;
 
@@ -23,7 +24,15 @@ public class PontoService {
 		Ponto ponto = null;
 		Funcionario funcionario = usuarioRepository.findById(idFuncionario).orElse(null);
 		if(null != funcionario){
-			ponto = pontoRepository.save(Ponto.builder().funcionario(funcionario).datahora(LocalDateTime.now()).build());
+			Integer rowCount = pontoRepository.countByFuncionario(funcionario);
+			if(rowCount == 0) {
+				ponto = pontoRepository.save(Ponto.builder().funcionario(funcionario).datahora(LocalDateTime.now()).tipo(TipoPontoEnum.E).build());
+			} else {
+				TipoPontoEnum tipo;
+				Ponto ultimoPonto = pontoRepository.findUltimoPontoByFuncionarioId(funcionario.getCodigo());
+				tipo = ultimoPonto.getTipo().equals(TipoPontoEnum.E) ? TipoPontoEnum.S : TipoPontoEnum.E;
+				ponto = pontoRepository.save(Ponto.builder().funcionario(funcionario).datahora(LocalDateTime.now()).tipo(tipo).build());
+			}
 		}
 		return ponto;
 	}	
